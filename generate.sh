91d50d61333de5444536795a5d4849ca806ecd9b
Jeffrey Sica <jeef111x@gmail.com>
2019-03-02 14:27:37 -0500

    add concurrently, start back/front end in one command

--- generate.sh
+++ generate.sh
@@ -5,4 +5,5 @@ release-notes \
     -github-token ${GITHUB_RELEASE_NOTES_TOKEN} \
     -start-sha fa6d17f400a94068b56b7d0f211f3cc9db290db6 \
     -end-sha f7eb5769c2dc4bc701802da639451d0f6cd56a18 \
+    -release-version=1.14 \
     -output /home/jeefy/Code/go/src/github.com/kubernetes/release/cmd/release-notes/relnotes.json
