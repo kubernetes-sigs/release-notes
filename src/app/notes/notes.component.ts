5457ac586a22e67d1cfb8976badb09bc9c167828
Jeffrey Sica <jeef111x@gmail.com>
2019-03-16 02:00:36 -0400

    remove artifacts from pagination rework

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input, EventEmitter, Output, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { NotesService } from './notes.service';
 import { Note } from './note';
 
