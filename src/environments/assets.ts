e480dae8bd6f82cafd6f1b84c4414ea1dd5a6477
Sascha Grunert <sgrunert@suse.com>
2019-10-07 19:58:53 +0200

    Sort assets to show latest notes first

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,8 +1,8 @@
 export const assets = [
-  'assets/release-notes-1.15.json',
-  'assets/release-notes-1.15.1.json',
-  'assets/release-notes-1.15.2.json',
-  'assets/release-notes-1.15.3.json',
-  'assets/release-notes-1.15.4.json',
   'assets/release-notes-1.16.json',
+  'assets/release-notes-1.15.4.json',
+  'assets/release-notes-1.15.3.json',
+  'assets/release-notes-1.15.2.json',
+  'assets/release-notes-1.15.1.json',
+  'assets/release-notes-1.15.json',
 ];
