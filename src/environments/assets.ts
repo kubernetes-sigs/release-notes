8a88775cad1b428f54317a9e2c63358e82087b4e
evillgenius75 <eddie.villalba@microsoft.com>
2019-10-11 15:44:19 -0500

    This is a combination of 6 commits.
    release note patches for v1.17.0-alpha.0
    
    move to assets directory
    
    release notes patch for v1.17.0-alpha.1
    
    release note patches for 1.17 alpha.0 to alpha.1
    
    updates to reference new release nomenclature
    
    Pretty json

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -5,4 +5,5 @@ export const assets = [
   'assets/release-notes-1.15.3.json',
   'assets/release-notes-1.15.4.json',
   'assets/release-notes-1.16.json',
+  'assets/release-notes-1.17.0-alpha.1.json',
 ];
