c5d4607c0fa1bc7396bd5813a313120a5ac83b99
Sascha Grunert <sgrunert@suse.com>
2019-10-16 09:07:26 +0200

    Add v1.16.1/2 release notes
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -5,4 +5,6 @@ export const assets = [
   'assets/release-notes-1.15.3.json',
   'assets/release-notes-1.15.4.json',
   'assets/release-notes-1.16.json',
+  'assets/release-notes-1.16.1.json',
+  'assets/release-notes-1.16.2.json',
 ];
