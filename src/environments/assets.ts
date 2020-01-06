ba0973b99d22a6a18ace48cc9557669f4739d219
evillgenius75 <evill_genius@outlook.com>
2020-01-06 13:27:14 -0600

    initial add of release notes to relnotes.k8s.io from k/k 1.18.0-alpha.1

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.18.0-alpha.1.json',
   'assets/release-notes-1.17.0.json',
   'assets/release-notes-1.16.4.json',
   'assets/release-notes-1.16.3.json',
