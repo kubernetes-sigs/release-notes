130927873920816c363fc17bf74b929bb9aabdb0
James Laverack <james.laverack@jetstack.io>
2020-02-13 22:35:04 +0000

    Patch relnotes.k8s.io with v1.18.0-alpha.5 release

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.18.0-alpha.5.json',
   'assets/release-notes-1.18.0-alpha.4.json',
   'assets/release-notes-1.18.0-alpha.3.json',
   'assets/release-notes-1.18.0-alpha.2.json',
