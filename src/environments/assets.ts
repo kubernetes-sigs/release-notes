af5f950b2c035f79a34d148a0854324b1916c810
Puerco <adolfo.garcia@uservers.net>
2020-02-24 11:50:00 -0600

    Patch relnotes.k8s.io with release v1.18.0-beta.0

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.18.0-beta.0.json',
   'assets/release-notes-1.18.0-alpha.5.json',
   'assets/release-notes-1.18.0-alpha.4.json',
   'assets/release-notes-1.18.0-alpha.3.json',
