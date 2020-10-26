7da070cf1a2f96299db01a28c9659bbf4e879ad9
Wilson E. Husin <wilsonehusin@gmail.com>
2020-10-26 09:22:45 -0700

    Patch relnotes.k8s.io with release v1.20.0-alpha.3
    Signed-off-by: Wilson E. Husin <wilsonehusin@gmail.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.20.0-alpha.3.json',
   'assets/release-notes-1.19.3.json',
   'assets/release-notes-1.20.0-alpha.2.json',
   'assets/release-notes-1.18.10.json',
