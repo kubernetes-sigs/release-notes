7a28d3d43ce6af5c20646c3ba87ac14a11747164
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2020-11-12 13:39:49 -0600

    Patch relnotes.k8s.io with release v1.18.12
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.18.12.json',
   'assets/release-notes-1.17.14.json',
   'assets/release-notes-1.20.0-beta.1.json',
   'assets/release-notes-1.20.0-alpha.3.json',
