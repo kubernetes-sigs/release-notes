f7d599a52805f0fdb4e54bb030ef0f5d34dff16b
Chris Carty <chriscartydev@gmail.com>
2020-12-08 08:15:27 -0500

    added google storage module and updated assets.ts to use it

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,92 +1,32 @@
-import { async } from 'rxjs';
+declare var require : any;
 
-declare var gapi : any;
+function getFiles(bucketName = 'kubernetes-release-gcb') {
+  // [START storage_list_files]
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+  // const bucketName = 'Name of a bucket, e.g. my-bucket';
 
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
 
-      // 'https://www.googleapis.com/download/storage/v1/b/relnotes-assests/o/release-notes-1.14.0.json?generation=1596471832663965&alt=media',
-      // 'https://content-storage.googleapis.com/download/storage/v1/b/relnotes-assests/o/release-notes-1.14.1.json?generation=1596471832426869&alt=media',
-  // 'assets/release-notes-1.19.0-rc.3.json',
-  // 'assets/release-notes-1.19.0-rc.2.json',
-  // 'assets/release-notes-1.18.6.json',
-  // 'assets/release-notes-1.17.9.json',
-  // 'assets/release-notes-1.16.13.json',
-  // 'assets/release-notes-1.19.0-rc.1.json',
-  // 'assets/release-notes-1.19.0-rc.0.json',
-  // 'assets/release-notes-1.17.8.json',
-  // 'assets/release-notes-1.17.7.json',
-  // 'assets/release-notes-1.17.6.json',
-  // 'assets/release-notes-1.17.5.json',
-  // 'assets/release-notes-1.18.5.json',
-  // 'assets/release-notes-1.18.4.json',
-  // 'assets/release-notes-1.17.4.json',
-  // 'assets/release-notes-1.18.3.json',
-  // 'assets/release-notes-1.17.3.json',
-  // 'assets/release-notes-1.19.0-beta.2.json',
-  // 'assets/release-notes-1.19.0-beta.1.json',
-  // 'assets/release-notes-1.18.2.json',
-  // 'assets/release-notes-1.19.0-beta.0.json',
-  // 'assets/release-notes-1.18.1.json',
-  // 'assets/release-notes-1.19.0-alpha.3.json',
-  // 'assets/release-notes-1.19.0-alpha.2.json',
-  // 'assets/release-notes-1.18.0.json',
-  // 'assets/release-notes-1.17.2.json',
-  // 'assets/release-notes-1.17.1.json',
-  // 'assets/release-notes-1.17.0.json',
-  // 'assets/release-notes-1.16.6.json',
-  // 'assets/release-notes-1.16.5.json',
-  // 'assets/release-notes-1.16.4.json',
-  // 'assets/release-notes-1.16.3.json',
-  // 'assets/release-notes-1.16.2.json',
-  // 'assets/release-notes-1.16.1.json',
-  // 'assets/release-notes-1.16.0.json',
-  // 'assets/release-notes-1.15.9.json',
-  // 'assets/release-notes-1.15.8.json',
-  // 'assets/release-notes-1.15.7.json',
-  // 'assets/release-notes-1.15.6.json',
-  // 'assets/release-notes-1.15.5.json',
-  // 'assets/release-notes-1.15.4.json',
-  // 'assets/release-notes-1.15.3.json',
-  // 'assets/release-notes-1.15.2.json',
-  // 'assets/release-notes-1.15.1.json',
-  // 'assets/release-notes-1.15.0.json',
-  // 'assets/release-notes-1.14.10.json',
-  // 'assets/release-notes-1.14.9.json',
-  // 'assets/release-notes-1.14.8.json',
-  // 'assets/release-notes-1.14.7.json',
-  // 'assets/release-notes-1.14.6.json',
-  // 'assets/release-notes-1.14.5.json',
-  // 'assets/release-notes-1.14.4.json',
-  // 'assets/release-notes-1.14.3.json',
-  // 'assets/release-notes-1.14.2.json',
-  // 'assets/release-notes-1.14.1.json',
-  // 'assets/release-notes-1.14.0.json',
+  // Creates a client
+  const storage = new Storage();
+  var files = [];
+  async function listFiles() {
+    // Lists files in the bucket
+    const [files] = await storage.bucket(bucketName).getFiles();
 
-function getNotes() {
+    console.log('Files:');
+    files.forEach(file => {
+      console.log(file.name);
+      files.Push(file.name);
+    });
+  }
 
-  var notes = [];
-  // 2. Initialize the JavaScript client library.
-  gapi.client.init({
-    'apiKey': '<Your API Key>',
-    // clientId and scope are optional if auth is not required.
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.request({
-      'path': 'https://storage.googleapis.com/storage/v1/b/relnotes-assests/o',
-    })
-  }).then(function(response) {
-    console.log(response.result);
-    for (var i = 0; i < response.result.items.length; i++) {
-      notes.push(response.result.items[i].mediaLink)
-    }
+  listFiles().catch(console.error);
+  return files;
+  // [END storage_list_files]
+}
 
-    return notes
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
-  });
-};
-// 1. Load the JavaScript client library.
-
-// export default async () => {
-//   return notes start();
-// }
-export const assets = getNotes()
+export const assets = getFiles()
