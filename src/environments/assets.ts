5aa30db045ac1c684030f03cb7c367012b765a08
cici37 <cicih@google.com>
2021-12-06 10:39:49 -0800

    Patch relnotes.k8s.io with release v1.23.0-rc.1
    Signed-off-by: cici37 <cicih@google.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.23.0-rc.1.json',
   'assets/release-notes-1.23.0-rc.0.json',
   'assets/release-notes-1.23.0-beta.0.json',
   'assets/release-notes-1.23.0-alpha.4.json',
