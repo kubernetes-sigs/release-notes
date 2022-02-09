d5ff0a5daa5e1d56c7f3e0025c7a6adc94d5a2ce
Carlos Santana <csantana23@gmail.com>
2022-02-09 18:49:59 -0500

    Patch relnotes.k8s.io with release v1.21.7
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.21.7.json',
   'assets/release-notes-1.24.0-alpha.2.json',
   'assets/release-notes-1.24.0-alpha.1.json',
   'assets/release-notes-1.23.0.json',
