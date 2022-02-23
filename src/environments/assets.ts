14a6f854ef41a5c05a543390a8f7709f1a2a6bd6
Francois LP <francois.le.pape@gmail.com>
2022-02-23 23:31:47 +0100

    Patch relnotes.k8s.io with release v1.22.7
    Signed-off-by: Francois LP <francois.le.pape@gmail.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.22.7.json',
   'assets/release-notes-1.24.0-alpha.3.json',
   'assets/release-notes-1.24.0-alpha.2.json',
   'assets/release-notes-1.24.0-alpha.1.json',
