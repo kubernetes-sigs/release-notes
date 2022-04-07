4c67031796c49fcd9911b85612985a8992749dd0
Lucas Dwyer <lucasdwyer@pm.me>
2022-04-07 13:19:11 -0500

    Patch relnotes.k8s.io with release v1.24.0-beta.0
    Signed-off-by: Lucas Dwyer <lucasdwyer@pm.me>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.24.0-beta.0.json',
   'assets/release-notes-1.24.0-alpha.4.json',
   'assets/release-notes-1.22.7.json',
   'assets/release-notes-1.24.0-alpha.3.json',
