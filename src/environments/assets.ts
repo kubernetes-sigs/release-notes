069903aba8191b2c1b8d338a12892bc01af2e575
Rodolfo Martínez Vega <rodomar@outlook.com>
2022-08-17 13:12:42 -0500

    Patch relnotes.k8s.io with release v1.25.0-alpha.0
    Signed-off-by: Rodolfo Martínez Vega <rodomar@outlook.com>
    (cherry picked from commit d4233d0e0a3d48d70b8b9b85cdffa9f7a318439d)

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -4,6 +4,7 @@ export const assets = [
   'assets/release-notes-1.25.0-alpha.3.json',
   'assets/release-notes-1.25.0-alpha.2.json',
   'assets/release-notes-1.25.0-alpha.1.json',
+  'assets/release-notes-1.25.0-alpha.0.json',
   'assets/release-notes-1.24.0-rc.0.json',
   'assets/release-notes-1.24.0-beta.0.json',
   'assets/release-notes-1.24.0-alpha.4.json',
