84e543fc4b059c6cc65ed73b293c3438e4b802b9
Frederico Muñoz <fsmunoz@gmail.com>
2023-08-13 14:56:51 +0100

    Patch relnotes.k8s.io with release v1.28.0-rc.1
    Signed-off-by: Frederico Muñoz <fsmunoz@gmail.com>

--- src/environments/assets.ts
+++ src/environments/assets.ts
@@ -1,4 +1,5 @@
 export const assets = [
+  'assets/release-notes-1.28.0-rc.1.json',
   'assets/release-notes-1.28.0-rc.0.json',
   'assets/release-notes-1.28.0-beta.0.json',
   'assets/release-notes-1.28.0-alpha.4.json',
